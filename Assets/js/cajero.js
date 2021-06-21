074e4f493180dca3fe1e5608c43921b5fde0f24e
Yann <metalyanni@gmail.com>
2021-06-20 19:28:58 -0500

    Ejercicio del cajero. Se maneja DOM donde recibimos datos desde un input del HTML, lo manejamos y volvemos a escribir en el HTML

--- Assets/js/cajero.js
+++ Assets/js/cajero.js
@@ -54,3 +54,8 @@ function billetes(cantidad){
 //var cantidad = 1821;
 //billetes(cantidad);
 
+
+
+/* Recibes la tarea de construir un sistema de alarma para una fábrica de champús. Tu sistema recibe las siguientes variables: temperatura ambiente, humedad ambiental, número de trabajadores en la planta y nivel de líquido en los tanques. La alarma debe sonar siempre que la temperatura ambiente sea superior o inferior a los umbrales, si la humedad es superior al 50%, si hay más de 100 trabajadores en la planta al mismo tiempo, o si el nivel del líquido es superior al 90% (para evitar el desbordamiento).
+umbral superior 90% umbral inferior 30%
+ */
